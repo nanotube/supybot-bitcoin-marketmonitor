f8b0f1f406d79d1e3d3800f2940517a30e269346
Daniel Folkinshteyn <nanotube@users.sourceforge.net>
2011-05-04 18:28:28 -0400

    GPG: add ruby signer helper script thanks mattsmith for the contribution

---
+++ GPG/helperscripts/gpgsigner.rb
@@ -0,0 +1,16 @@
+require 'xmlrpc/client'
+
+# Sign it
+response = ""
+STDOUT.sync = true
+IO.popen("echo " + ARGV[0] + " | gpg --clearsign") do |pipe|
+  pipe.sync = true
+  while str = pipe.gets
+    response += str
+  end
+end
+
+# Post it
+server = XMLRPC::Client.new2("http://paste.pocoo.org/xmlrpc/")
+pasteid = server.call("pastes.newPaste",'text',response)
+puts 'http://paste.pocoo.org/raw/' + pasteid
