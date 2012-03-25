8583a617a615ef2714cd34e585a6526bdf05059f
Daniel Folkinshteyn <nanotube@users.sourceforge.net>
2012-03-25 14:56:05 -0400

    OTCWebsite: use json_encode to rip out a lot of manual code and produce actually correct json.
    thanks graingert for the suggestion.

--- OTCWebsite/querytojson.php
+++ OTCWebsite/querytojson.php
@@ -1,6 +1,8 @@
 <?php
 # Thanks to http://code-slim-jim.blogspot.com/2011/07/php-sqlite-to-json-converter-page.html
 # for the nice snippet.
+# Thanks to graingert for pointing out that json_encode would eliminate a lot of code and
+# produce actually correct json (escape control chars, etc.)
 function jsonOutput($query)
 {
     header('Content-type: application/json');
@@ -10,19 +12,9 @@ function jsonOutput($query)
     {
         if($ofirst) $ofirst = false;
         else       print ",\n";
-
-        $first = true;
-        print "{";
-        foreach ($row as $key => $value)
-        {
-            if($first) $first = false;
-            else       print ",";
-            if(is_numeric($value))
-                print '"' . $key . '": ' . $value;
-            else
-                print '"' . $key . '": "' . $value . '"';
-        }
-        print "}";
+        
+        $rowjs = json_encode($row);
+        print $rowjs;
     }
     print "\n]\n";
 }
