c2c5d7a122b97678a1aaa93361ed73ace1012d02
Daniel Folkinshteyn <nanotube@users.sourceforge.net>
2013-03-28 19:03:38 -0400

    BotBackup: new plugin allowing for easy backup bot creation.

---
+++ BotBackup/plugin.py
@@ -0,0 +1,60 @@
+###
+# Copyright (c) 2013, Daniel Folkinshteyn
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#   * Redistributions of source code must retain the above copyright notice,
+#     this list of conditions, and the following disclaimer.
+#   * Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions, and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#   * Neither the name of the author of this software nor the name of
+#     contributors to this software may be used to endorse or promote products
+#     derived from this software without specific prior written consent.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+###
+
+import supybot.utils as utils
+from supybot.commands import *
+import supybot.plugins as plugins
+import supybot.ircutils as ircutils
+import supybot.callbacks as callbacks
+
+
+class BotBackup(callbacks.Plugin):
+    """This plugin lets you run a live instance of a backup bot. It will keep
+    the backup bot silent unless all the precedent bots are offline."""
+    threaded=True
+    
+    def outFilter(self, irc, msg):
+        if msg.command == 'PRIVMSG' and \
+                irc.isChannel(msg.args[0]) and \
+                msg.inReplyTo is not None: # 'say' msgs don't have an inReplyTo
+            for pn in self.registryValue('precedentBotNicks'):
+                if pn in irc.state.channels[msg.args[0]].users:
+                    quiet = True
+                    break
+            else:
+                quiet = False
+        if quiet:
+            return None
+        return msg
+
+Class = BotBackup
+
+
+# vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
