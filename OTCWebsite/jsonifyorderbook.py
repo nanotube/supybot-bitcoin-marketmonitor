d6be91817c272027c3ed095c524161c38c551671
Daniel Folkinshteyn <nanotube@users.sourceforge.net>
2013-09-22 23:20:58 -0400

    OTCWebsite: deal with encoding issues for orderbook jsonifier.

--- OTCWebsite/jsonifyorderbook.py
+++ OTCWebsite/jsonifyorderbook.py
@@ -5,6 +5,7 @@ class JsonifyOrderBook:
     def __init__(self, orderbook_db_path, json_path):
         self.json_path = json_path
         self.db = sqlite3.connect(orderbook_db_path)
+        self.db.text_factory = str
 
     def run(self):
         i = True
@@ -16,7 +17,10 @@ class JsonifyOrderBook:
             d = dict(zip(['id','created_at','refreshed_at','buysell','nick','amount','thing','price','otherthing','notes'],row))
             if not i:
                 f.write(',')
-            f.write(json.dumps(d))
+            try:
+                f.write(json.dumps(d, encoding='latin1'))
+            except:
+                pass # if it's not decodable, skip it without further ado.
             i = False
         f.write(']')
 
