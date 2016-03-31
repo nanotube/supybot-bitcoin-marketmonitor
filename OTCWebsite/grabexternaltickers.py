4f20c0ee9145b71b73671f8329d26abf23c94302
Thomas Ward <teward@ubuntu.com>
2016-03-31 14:02:04 -0400

    OTCWebsite: Gox is dead.

--- OTCWebsite/grabexternaltickers.py
+++ OTCWebsite/grabexternaltickers.py
@@ -41,37 +41,11 @@ def get_bitstamp_ticker():
         stdticker = {'error':'something failed'}
     return stdticker
 
-def get_mtgox_ticker():
-    try:
-        json_data = urllib2.urlopen("https://data.mtgox.com/api/2/BTC%s/money/ticker" % ('USD',)).read()
-        ticker = json.loads(json_data)
-        ftj = urllib2.urlopen("http://data.mtgox.com/api/2/BTC%s/money/ticker_fast" % ('USD',)).read()
-        tf = json.loads(ftj)
-        if ticker['result'] != 'error' and tf['result'] != 'error': # use fast ticker where available
-            ticker['data']['buy']['value'] = tf['data']['buy']['value']
-            ticker['data']['sell']['value'] = tf['data']['sell']['value']
-            ticker['data']['last']['value'] = tf['data']['last']['value']
-        if ticker['result'] == 'error':
-             stdticker = {'error':ticker['error']}
-        else:
-            stdticker = {'bid': ticker['data']['buy']['value'],
-                                'ask': ticker['data']['sell']['value'],
-                                'last': ticker['data']['last']['value'],
-                                'vol': ticker['data']['vol']['value'],
-                                'low': ticker['data']['low']['value'],
-                                'high': ticker['data']['high']['value'],
-                                'avg': ticker['data']['vwap']['value']}
-    except:
-        stdticker = {'error':'something failed'}
-    return stdticker
-
 def write_json(data, fname):
     f = open(fname, 'w')
     f.write(json.dumps(data))
     f.close()
 
 if __name__ == '__main__':
-    goxtic = get_mtgox_ticker()
-    write_json(goxtic, 'mtgox.json')
     btsptic = get_bitstamp_ticker()
     write_json(btsptic, 'bitstamp.json')
