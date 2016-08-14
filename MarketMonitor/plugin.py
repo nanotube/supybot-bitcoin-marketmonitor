a125287d2996fa87866301efd6874e7072dd8e58
Daniel Folkinshteyn <nanotube@users.sourceforge.net>
2016-08-14 17:32:59 -0400

    MarketMonitor: remove now-useless old code

--- MarketMonitor/plugin.py
+++ MarketMonitor/plugin.py
@@ -31,7 +31,6 @@
 
 import decimal
 import locale
-import telnetlib
 import threading
 import time
 import re
@@ -58,7 +57,6 @@ class MarketMonitor(callbacks.Plugin):
     def __init__(self, irc):
         self.__parent = super(MarketMonitor, self)
         self.__parent.__init__(irc)
-        #self.conn = telnetlib.Telnet()
         self.e = threading.Event()
         self.started = threading.Event()
         self.data = ""
@@ -84,45 +82,8 @@ class MarketMonitor(callbacks.Plugin):
         if not self.started.isSet() and irc.network == self.registryValue('network') and self.registryValue('autostart'):
             self._start(irc)
 
-    #def _reconnect(self, repeat=True):
-        #while not self.e.isSet():
-            #try:
-                #self.conn.close()
-                #self.conn.open(self.registryValue('server'),
-                                    #self.registryValue('port'))
-                #self.nextsend = time.time()
-                #return True
-            #except Exception, e:
-                ## this may get verbose, but let's leave this in for now.
-                #self.log.error('MarketMonitor: reconnect error: %s: %s' % \
-                            #(e.__class__.__name__, str(e)))
-                #if not repeat:
-                    #return False
-                #time.sleep(5)
-
     def _monitor(self, irc):
         while not self.e.isSet():
-            #try:
-                #lines = self.conn.read_very_eager()
-            #except Exception, e:
-                #self.log.error('Error in MarketMonitor reading telnet: %s: %s' % \
-                            #(e.__class__.__name__, str(e)))
-                #self._reconnect()
-                #continue
-            #try:
-                #if irc.getCallback('Services').identified and lines: #Make sure you're running the Services plugin, and are identified!
-                    #lines = lines.split("\n")
-                    #self._parse(lines)
-            #except Exception, e:
-                #self.log.error('Error in MarketMonitor parsing: %s: %s' % \
-                            #(e.__class__.__name__, str(e)))
-                #continue # keep going no matter what
-            #try:
-                #self._parse() # TODO grab a chunk of lines from queue and send it over
-            #except Exception, e:
-                #self.log.error('Error in MarketMonitor parsing: %s: %s' % \
-                            #(e.__class__.__name__, str(e)))
-                #continue # keep going no matter what
             try:
                 chunk = self.q.get(True, 10)
                 k,v = chunk.items()[0]
@@ -141,9 +102,6 @@ class MarketMonitor(callbacks.Plugin):
                             for chan in self.registryValue('channels'):
                                 irc.queueMsg(ircmsgs.privmsg(chan, output))
                         self.nextsend = time.time()+(conf.supybot.protocols.irc.throttleTime() * len(outputs))
-                    #if time.time() - self.nextsend > 300: # 5 minutes no data
-                        #self.log.info("MarketMonitor reconnecting due to silent feed.")
-                        #self._reconnect() # must mean feed is quietly dead, as sometimes happens.
                     self.marketdata = {}
                     self.raw = []
             except Exception, e:
@@ -152,48 +110,6 @@ class MarketMonitor(callbacks.Plugin):
                 continue # keep going no matter what
             time.sleep(0.01)
         self.started.clear()
-
-    def _parse(self, msgs):
-        # Stitching of messages
-        if len(msgs) == 1:
-            self.data += msgs[0]
-            return
-        msgs[0] = self.data + msgs[0]
-        self.data = ""
-        if not msgs[-1] == "":
-            self.data = msgs[-1]
-
-        msgs = msgs[:-1]
-
-        if self.registryValue('format') == 'raw':
-            self.raw.extend(msgs)
-
-        #[{"timestamp": 1302015318, "price": "0.7000", "volume": "0.27", "currency": "USD", "symbol": "btcexUSD"}]
-
-        # Parsing of messages
-        for data in msgs:
-            try:
-                d = json.loads(data)
-                for needed in "timestamp", "price", "volume", "symbol":
-                    assert needed in d
-                market, currency = re.match(r"^([a-z0-9]+)([A-Z]+)$", d["symbol"]).groups()
-                if market in self.registryValue('marketsBlacklist'):
-                    continue
-                if len(self.registryValue('marketsWhitelist')) != 0 and market not in self.registryValue('marketsWhitelist'):
-                    continue
-                volume = decimal.Decimal(str(d["volume"]))
-                price = decimal.Decimal(str(d["price"]))
-                stamp = decimal.Decimal(str(d["timestamp"]))
-                if (market, currency) not in self.marketdata:
-                    self.marketdata[(market, currency)] = []
-                self.marketdata[(market, currency)].append((volume, price, stamp))
-            except Exception, e:
-                # we really want to keep going no matter what data we get
-                self.log.error('Error in MarketMonitor parsing: %s: %s' % \
-                                (e.__class__.__name__, str(e)))
-                self.log.error('MarketMonitor: Unrecognized data: %s' % data)
-                self.data = ""
-                return False
 
     def _format(self):
         if self.registryValue('format') == 'raw':
@@ -234,7 +150,6 @@ class MarketMonitor(callbacks.Plugin):
 
     def die(self):
         self.e.set()
-        #self.conn.close()
         self.__parent.die()
 
     def _start(self, irc):
