c02c57b3ad18826f77f8e01ef62087f0922bd872
Daniel Folkinshteyn <nanotube@users.sourceforge.net>
2018-07-02 16:43:55 -0400

    BitcoinData: change api provider to chain.so, since old ones broke backwards-compatibility

--- BitcoinData/plugin.py
+++ BitcoinData/plugin.py
@@ -40,6 +40,7 @@ import re
 import time
 import math
 import urllib2
+import decimal
 
 opener = urllib2.build_opener()
 opener.addheaders = [('User-agent', 'Mozilla/5.0 (X11; Linux x86_64; rv:22.0) Gecko/20100101 Firefox/22.0')]
@@ -66,19 +67,30 @@ class BitcoinData(callbacks.Plugin):
     threaded = True
 
     def _grabapi(self, apipaths):
-        sources = ['http://blockchain.info','http://blockexplorer.com', ]
+        sources = ['https://chain.so', ]
         urls = [''.join(t) for t in zip(sources, apipaths)]
         for url in urls:
             try:
-                data = urlopen(url, timeout=5).read()
-                return data
+                req = urllib2.Request(url, headers={'User-Agent' : "I am a Browser"})
+                data = urlopen(req, timeout=5).read()
+                data = json.loads(data)
+                if data['status'] == 'success':
+                    return data['data']
             except:
                 continue
         else:
             return None
 
+    def _netinfo(self):
+        data = self._grabapi(['/api/v2/get_info/BTC'])
+        return data
+
     def _blocks(self):
-        data = self._grabapi(['/q/getblockcount']*2)
+        data = self._netinfo()['blocks']
+        return data
+
+    def _diff(self):
+        data = self._netinfo()['mining_difficulty']
         return data
 
     def blocks(self, irc, msg, args):
@@ -91,55 +103,25 @@ class BitcoinData(callbacks.Plugin):
             return
         irc.reply(data)
     blocks = wrap(blocks)
-        
-    def _rawblockbyhash(self, blockhash):
-        data = self._grabapi(['/rawblock/%s' % blockhash]*2)
+
+    def _getrawblock(self, blockid):
+        data = self._grabapi(['/api/v2/get_block/BTC/%s' % blockid])
         return data
         
-    def _rawblockbynum(self, blocknum):
-        try:
-            data = urlopen('http://blockexplorer.com/b/%s' % blocknum, timeout=5).read()
-            m = re.search(r'href="(/rawblock/[0-9a-f]+)"', data)
-            bbeurl = m.group(1)
-        except:
-            bbeurl = 'doesnotexist'
-        data = self._grabapi(['/block-height/%s?format=json' % blocknum, bbeurl, ])
-        try:
-            j = json.loads(data)
-            if 'blocks' in j.keys():
-                j = j['blocks'][0]
-            return j
-        except:
-            return None
-
-    def _blockdiff(self, blocknum):
-        block = self._rawblockbynum(blocknum)
-        try:
-            diffbits = block['bits']
-            hexbits = hex(diffbits)
-            target = int(hexbits[4:], 16) * 2 ** (8 * (int(hexbits[2:4], 16) - 3))
-            maxtarget = float(0x00000000FFFF0000000000000000000000000000000000000000000000000000)
-            diff = maxtarget / target
-            return diff
-        except:
-            return None
+    def _blockdiff(self, blockid):
+        block = self._getrawblock(blockid)
+        return block['mining_difficulty']
 
     def blockdiff(self, irc, msg, args, blocknum):
-        '''<block number>
+        '''<block number | block hash>
         
-        Get difficulty for specified <block number>.'''
-        #data = self._grabapi(['b/%s' % blocknum, 'rawblock/%s' % blocknum])
-        # first, let's try to grab from bbe, we need blockhash first
+        Get difficulty for specified <block number> or <block hash>.'''
         diff = self._blockdiff(blocknum)
         if diff is None:
             irc.error("Failed to retrieve data. Try again later.")
             return
         irc.reply(diff)
-    blockdiff = wrap(blockdiff, ['positiveInt'])
-
-    def _diff(self):
-        data = self._grabapi(['/q/getdifficulty']*2)
-        return data
+    blockdiff = wrap(blockdiff, ['something'])
 
     def diff(self, irc, msg, args):
         '''takes no arguments
@@ -152,43 +134,12 @@ class BitcoinData(callbacks.Plugin):
         irc.reply(data)
     diff = wrap(diff)
 
-    def _hextarget(self, blocknum):
-        block = self._rawblockbynum(blocknum)
-        try:
-            diffbits = block['bits']
-            hexbits = hex(diffbits)
-            target = int(hexbits[4:], 16) * 2 ** (8 * (int(hexbits[2:4], 16) - 3))
-            target = hex(target)[2:-1]
-            target = '0'*(64-len(target)) + target
-            return target.upper()
-        except:
-            return None
-
-    def hextarget(self, irc, msg, args, blocknum):
-        '''[<block number>]
-        
-        get the hex target for current block.
-        if optional block number is provided, get hex target for that block height.
-        '''
-        if blocknum is None:
-            blocknum = self._blocks()
-        target = self._hextarget(blocknum)
-        if target is None:
-            irc.error("Failed to retrieve data. Try again later.")
-            return
-        irc.reply(target)
-    hextarget = wrap(hextarget, [optional('positiveInt')])
-
     def _bounty(self):
-        data = self._grabapi(['/q/bcperblock']*2)
-        try:
-            if int(data) > 50:
-                return int(data) / 100000000
-            else:
-                return int(data)
-        except:
-            return None
-
+        blocks = self._blocks()
+        retargets = int(blocks/210000)
+        bounty = 50.0 / 2**retargets
+        return bounty
+        
     def bounty(self, irc, msg, args):
         '''takes no arguments
         
@@ -201,13 +152,13 @@ class BitcoinData(callbacks.Plugin):
     bounty = wrap(bounty)
 
     def _gentime(self, hashrate, difficulty):
-        gentime = 2**48/65535*difficulty/hashrate/1000000
+        gentime = 2**48/65535*difficulty/hashrate/1000000000000
         return gentime
 
     def gentime(self, irc, msg, args, hashrate, difficulty):
         '''<hashrate> [<difficulty>]
         
-        Calculate expected time to generate a block using <hashrate> Mhps,
+        Calculate expected time to generate a block using <hashrate> Thps,
         at current difficulty. If optional <difficulty> argument is provided, expected
         generation time is for supplied difficulty.
         '''
@@ -218,14 +169,14 @@ class BitcoinData(callbacks.Plugin):
                 irc.error("Failed to fetch current difficulty. Try again later or supply difficulty manually.")
                 return
         gentime = self._gentime(hashrate, difficulty)
-        irc.reply("The average time to generate a block at %s Mhps, given difficulty of %s, is %s" % \
+        irc.reply("The average time to generate a block at %s Thps, given difficulty of %s, is %s" % \
                 (hashrate, difficulty, utils.timeElapsed(gentime)))
     gentime = wrap(gentime, ['positiveFloat', optional('positiveFloat')])
 
     def genrate(self, irc, msg, args, hashrate, difficulty):
         '''<hashrate> [<difficulty>]
         
-        Calculate expected bitcoin generation rate using <hashrate> Mhps,
+        Calculate expected bitcoin generation rate using <hashrate> Thps,
         at current difficulty. If optional <difficulty> argument is provided, expected
         generation time is for supplied difficulty.
         '''
@@ -241,7 +192,7 @@ class BitcoinData(callbacks.Plugin):
         except:
             irc.error("Failed to retrieve current block bounty. Try again later.")
             return
-        irc.reply("The expected generation output, at %s Mhps, given difficulty of %s, is %s BTC "
+        irc.reply("The expected generation output, at %s Thps, given difficulty of %s, is %s BTC "
                 "per day and %s BTC per hour." % (hashrate, difficulty,
                             bounty*24*60*60/gentime,
                             bounty * 60*60/gentime))
@@ -254,7 +205,7 @@ class BitcoinData(callbacks.Plugin):
         This uses the block timestamp, so may be slightly off clock-time.
         """
         blocknum = self._blocks()
-        block = self._rawblockbynum(blocknum)
+        block = self._getrawblock(blocknum)
         try:
             blocktime = block['time']
             irc.reply("Time since last block: %s" % utils.timeElapsed(time.time() - blocktime))
@@ -269,27 +220,17 @@ class BitcoinData(callbacks.Plugin):
         except:
             estimate = None
         return estimate
-    
-    def _nethashsincelast(self):
-        try:
-            estimate = urlopen('http://blockexplorer.com/q/estimate').read()
-            estimate = float(estimate) / 139.696254564
-        except:
-            estimate = None
-        return estimate
-    
+        
     def nethash(self, irc, msg, args):
         '''takes no arguments
         
-        Shows the current estimate for total network hash rate, in Ghps.
+        Shows the current estimate for total network hash rate, in Thps.
         '''
-        data = self._nethash3d()
-        if data is None:
-            data = self._nethashsincelast()
+        data = self._netinfo()['hashrate']
         if data is None:
             irc.error("Failed to retrieve data. Try again later.")
             return
-        irc.reply(data)
+        irc.reply(float(data)/1000000000000)
     nethash = wrap(nethash)
 
     def diffchange(self, irc, msg, args):
@@ -303,12 +244,7 @@ class BitcoinData(callbacks.Plugin):
             diff3d = round(100*(diff3d/float(currdiff) - 1), 5)
         except:
             diff3d = None
-        try:
-            diffsincelast = self._nethashsincelast() * 139.696254564
-            diffsincelast = round(100*(diffsincelast/float(currdiff) - 1), 5)
-        except:
-            diffsincelast = None
-        irc.reply("Estimated percent change in difficulty this period | %s %% based on data since last change | %s %% based on data for last three days" % (diffsincelast, diff3d))
+        irc.reply("Estimated percent change in difficulty this period %s %% based on data for last three days" % (diff3d,))
     diffchange = wrap(diffchange)
     
     def estimate(self, irc, msg, args):
@@ -317,14 +253,10 @@ class BitcoinData(callbacks.Plugin):
         Shows next difficulty estimate.
         """
         try:
-            diff3d = self._nethash3d() * 139.696254564
+            diff3d = decimal.Decimal(self._nethash3d() * 139.696254564)
         except:
             diff3d = None
-        try:
-            diffsincelast = self._nethashsincelast() * 139.696254564
-        except:
-            diffsincelast = None
-        irc.reply("Next difficulty estimate | %s based on data since last change | %s based on data for last three days" % (diffsincelast, diff3d))
+        irc.reply("Next difficulty estimate %s based on data for last three days" % (diff3d,))
     estimate = wrap(estimate)
 
     def totalbc(self, irc, msg, args):
@@ -368,22 +300,6 @@ class BitcoinData(callbacks.Plugin):
                 (time.asctime(time.gmtime(time.time() + sectohalve)), utils.timeElapsed(sectohalve)))
     halfreward = wrap(halfreward)
 
-    def _nextretarget(self):
-        data = self._grabapi(['/q/nextretarget']*2)
-        return data
-        
-    def nextretarget(self, irc, msg, args):
-        """takes no arguments
-        
-        Shows the block number at which the next difficulty change will take place.
-        """
-        data = self._nextretarget()
-        if data is None or data == '':
-            irc.error("Failed to retrieve data. Try again later.")
-            return
-        irc.reply(data)
-    nextretarget = wrap(nextretarget)
-
     def _prevdiff(self):
         blocks = int(self._blocks())
         prevdiff = self._blockdiff(blocks - 2016)
@@ -415,88 +331,16 @@ class BitcoinData(callbacks.Plugin):
         irc.reply("%s" % (round((diff / prevdiff - 1) * 100, 5), ))
     prevdiffchange = wrap(prevdiffchange)
 
-    def _interval(self):
-        data = self._grabapi(['/q/interval']*2)
-        return data
-        
-    def interval(self, irc, msg, args):
-        """takes no arguments
-        
-        Shows average interval, in seconds, between last 1000 blocks.
-        """
-        data = self._interval()
-        if data is None or data == '':
-            irc.error("Failed to retrieve data. Try again later.")
-            return
-        irc.reply(data)
-    interval = wrap(interval)
-
-    def _timetonext(self):
-        try:
-            interval = float(self._interval())
-            blocks = float(self._blocks())
-            retarget = float(self._nextretarget())
-            return (retarget - blocks)*interval
-        except:
-            return None
-
-    def timetonext(self, irc, msg, args):
-        """takes no arguments
-        
-        Show estimated time to next difficulty change.
-        """
-        data = self._timetonext()
-        if data is None:
-            irc.error("Failed to retrieve data. Try again later.")
-            return
-        irc.reply("%s" % data)
-    timetonext = wrap(timetonext)
-
-    def bcstats(self, irc, msg, args):
-        """takes no arguments
-        
-        Shows a number of statistics about the state of the block chain.
-        """
-        blocks = self._blocks()
-        diff = self._diff()
-        try:
-            estimate = self._nethashsincelast() * 139.696254564
-        except:
-            estimate = None
-        try:
-            diffchange = round((estimate/float(diff) - 1)  * 100, 5)
-        except:
-            diffchange = None
-        nextretarget = self._nextretarget()
-        try:
-            blockstoretarget = int(nextretarget) - int(blocks)
-        except:
-            blockstoretarget = None
-        try:
-            timetonext = utils.timeElapsed(self._timetonext())
-        except:
-            timetonext = None        
-        
-        irc.reply("Current Blocks: %s | Current Difficulty: %s | "
-                "Next Difficulty At Block: %s | "
-                "Next Difficulty In: %s blocks | "
-                "Next Difficulty In About: %s | "
-                "Next Difficulty Estimate: %s | "
-                "Estimated Percent Change: %s" % (blocks, diff, 
-                        nextretarget, blockstoretarget, timetonext, 
-                        estimate, diffchange))
-    bcstats = wrap(bcstats)
-
 #math calc 1-exp(-$1*1000 * [seconds $*] / (2**32* [bc,diff]))
 
     def _genprob(self, hashrate, interval, difficulty):
-        genprob = 1-math.exp(-hashrate*1000000 * interval / (2**32* difficulty))
+        genprob = 1-math.exp(-hashrate*1000000000000 * interval / (2**32* difficulty))
         return genprob
 
     def genprob(self, irc, msg, args, hashrate, interval, difficulty):
         '''<hashrate> <interval> [<difficulty>]
         
-        Calculate probability to generate a block using <hashrate> Mhps,
+        Calculate probability to generate a block using <hashrate> Thps,
         in <interval> seconds, at current difficulty.
         If optional <difficulty> argument is provided, probability is for supplied difficulty.
         To provide the <interval> argument, a nested 'seconds' command may be helpful.
@@ -508,7 +352,7 @@ class BitcoinData(callbacks.Plugin):
                 irc.error("Failed to current difficulty. Try again later or supply difficulty manually.")
                 return
         gp = self._genprob(hashrate, interval, difficulty)
-        irc.reply("The probability to generate a block at %s Mhps within %s, given difficulty of %s, is %s" % \
+        irc.reply("The probability to generate a block at %s Thps within %s, given difficulty of %s, is %s" % \
                 (hashrate, utils.timeElapsed(interval), difficulty, gp))
     genprob = wrap(genprob, ['positiveFloat', 'positiveInt', optional('positiveFloat')])
 
@@ -522,7 +366,7 @@ class BitcoinData(callbacks.Plugin):
         try:
             difficulty = float(self._diff())
             nh = float(self._nethash3d())
-            gp = self._genprob(nh*1000, interval, difficulty)
+            gp = self._genprob(nh/1000, interval, difficulty)
         except:
             irc.error("Problem retrieving data. Try again later.")
             return
